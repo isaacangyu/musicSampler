aa9c8ba41e463ca33dde2df6085946dc77ebfc62
ronit127 <00ronitr@gmail.com>
2025-03-01 18:01:28 -0600

    testin

--- testin.py
+++ testin.py
@@ -1,4 +1,11 @@
 import blocks
 from blocks import Block
-b1 = Block(name= "drummer", start = 200, end = 700, block_type="Sample", audio_file="faded.mp3")
+
+def secToMs(sec):
+    return sec*1000
+
+b1 = Block(name= "drummer", start = secToMs(60.2), end = secToMs(61), block_type="Sample", audio_file="faded.mp3")
+b2 = Block(name= "drummer", start = secToMs(62), end = secToMs(63), block_type="Sample", audio_file="faded.mp3")
+b1.play()
+b2.play()
 b1.play()
